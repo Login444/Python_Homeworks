88f0f6bc34b096986a00878e5cca4571141ddd17
login4 <birukovartemij@gmail.com>
2023-04-24 21:34:52 +0300

    Решена задача 2 из ДЗ 6

---
+++ Homework6/HW_6_2.py
@@ -0,0 +1,18 @@
+# Определить индексы элементов массива (списка), значения которых принадлежат 
+# заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+
+list = [random.randint(-15,15) for i in range(20)]
+
+print(list)
+
+a = int(input('Задайте минимум: '))
+b = int(input('Задайте максимум: '))
+
+list_index = []
+
+for i in range(len(list)):
+    if a <= list[i] <= b:
+        list_index.append(i)
+print(list_index)
